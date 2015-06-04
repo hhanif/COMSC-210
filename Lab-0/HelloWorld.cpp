a9a09d2622d2006c36746ffb1233ff993860839b
Haris Hanif <hhanif@users.noreply.github.com>
2015-06-03 17:11:39 -0700

    Create HelloWorld.cpp

---
+++ Lab-0/HelloWorld.cpp
@@ -0,0 +1,21 @@
+// Lab 0b, The "Hello, World" Program
+// Programmer: Haris Hanif
+// Editor(s) used: Xcode
+// Compiler(s) used: Xcode
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  // print my name and this assignment's title
+  cout << "Lab 0b, The \"Hello, World\" Program \n";
+  cout << "Programmer: Haris Hanif\n";
+  cout << "Editor(s) used: Xcode\n";
+  cout << "Compiler(s) used: Xcode\n";
+  cout << "File: " << __FILE__ << endl;
+  cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl;
+    
+  // This code block prints the words "Hello World"
+  cout << "Hello World\n";
+
+}
